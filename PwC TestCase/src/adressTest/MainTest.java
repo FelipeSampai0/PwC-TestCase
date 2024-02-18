06f463ee584b44dd3eeb9ea9a4bf09140e19f22e
Felipe S. <felipe.sampaio214@gmail.com>
2024-02-18 03:54:56 -0300

    initial commit

---
+++ PwC TestCase/src/adressTest/MainTest.java
@@ -0,0 +1,20 @@
+package adressTest;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		//String[] testAdresses1 = new String[] {"Miritiba 339", "Babaçu 500", "Cambuí 804B"};		
+		//String[] testAdresses2 = new String[] {"Rio Branco 23", "Quirino dos Santos 23 b"};		
+		//String[] testAdresses3 = new String[] {"4, Rue de la République", "100 Broadway Av", "Calle Sagasta, 26", "Calle 44 No 1991"};
+
+	}
+	
+	public static String[] adressTest(String input) {
+		
+		String[] adressStrings = new String[2];
+		
+		return adressStrings;
+	}
+
+}
