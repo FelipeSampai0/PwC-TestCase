3003db11026823db46ac4b90cf58d1e4936f420d
Felipe S. <felipe.sampaio214@gmail.com>
2024-02-19 16:27:19 -0300

    First working version, basic adress cases.

--- src/adressTest/MainTest.java
+++ src/adressTest/MainTest.java
@@ -1,20 +1,49 @@
 package adressTest;
 
+import java.util.ArrayList;
+import java.util.regex.*;
+
 public class MainTest {
+	
+	static ArrayList<String> Streets = new ArrayList<String>();
+	static ArrayList<String> Numbers = new ArrayList<String>();
 
 	public static void main(String[] args) {
 		
-		//String[] testAdresses1 = new String[] {"Miritiba 339", "Babaçu 500", "Cambuí 804B"};		
-		//String[] testAdresses2 = new String[] {"Rio Branco 23", "Quirino dos Santos 23 b"};		
-		//String[] testAdresses3 = new String[] {"4, Rue de la République", "100 Broadway Av", "Calle Sagasta, 26", "Calle 44 No 1991"};
-
+		String[] testAdresses1 = new String[] {"Miritiba 339", "Babaçu 500", "Cambuí 804B"};		
+		String[] testAdresses2 = new String[] {"Rio Branco 23", "Quirino dos Santos 23 b"};		
+		String[] testAdresses3 = new String[] {"4, Rue de la République", "100 Broadway Av", "Calle Sagasta, 26", "Calle 44 No 1991"};
+		
+		for (String e : testAdresses1) {			
+			separateAdressData(e);			
+		}
+		
+		
+		System.out.println(Streets);
+		System.out.println(Numbers);
 	}
 	
-	public static String[] adressTest(String input) {
+	public static void separateAdressData(String input) {	
 		
-		String[] adressStrings = new String[2];
+		String streetFound = new String();		
+		String numberFound = new String();
+
+		Pattern adressPattern = Pattern.compile("\\b[a-zA-Z]+\\b");
+		Pattern numberPattern = Pattern.compile("\\b(?:\\d+\\w|No)\\b");
 		
-		return adressStrings;
+		Matcher streetMatcher = adressPattern.matcher(input);
+		while(streetMatcher.find()) {
+			streetFound += streetMatcher.group();			
+		}	
+		
+		Matcher numberMatcher = numberPattern.matcher(input);		
+		while(numberMatcher.find()) {
+			numberFound += numberMatcher.group();
+		}
+		
+		Streets.add(streetFound);
+		Numbers.add(numberFound);
+	
 	}
 
 }
